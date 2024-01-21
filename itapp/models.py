dd3fcb7ac9566bdfce75d2869353381ae44d8b5d
arontz85 <aron.zeru85@gmail.com>
2024-01-20 22:43:58 -0700

    Changes has been made

--- itapp/models.py
+++ itapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-class Students(models.Model):
+class Users(models.Model):
     name = models.CharField(max_length=80)
     age = models.IntegerField()
